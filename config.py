50d383fe27a27baea4297e303a459d12437a2f07
sancaktaroglu <91407837+sancaktaroglu@users.noreply.github.com>
2022-12-11 21:20:14 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36"}
+headers['cookie'] = "_abck=44AF443D5955AB90BC840ECFDAA05E68~0~YAAQlIYUAkhK6FKBAQAAHXF4owiQOCUL3I5hgOm7/diBIKLk2xm/N213IK19wNmi/11AI9GCR3ovY5doZSa14f13ajFj5L0Tu+sfYmN6j2n2LG9d9DCQ+Qt8534NiSytlnkCLr/2MMytIRM81MopYR7NJyl/EBkbVVGIJuR5tuzn1tKHNeefRURIUg76v9o372Owx8BuOATDbp677ltMi/u7Sb8pmORL24kClG8JMzG0rRmtj6gPgUkHsKl0NXaBKeiabyQSL66cQ7h0ehGaSPRQFQ8zsTi8q4DA9FAkBICvs4ce7TC8rwZzoUbz9bOtVQuWetf00rVwJQTLQO0NhWKc7rFdT8j1HeKzX+UyDs0NAl/BQo284SwXAeLDl+VrDK12gfv2AkLGUfUso3INnPqOOJWiwN474N0cupuU~-1~||-1||~-1"
+
+NE = "NE"
+NA = "n/a"
+
+months = {'Ocak': "01", 'Şubat': "02", 'Mart': "03", 'Nisan': "04", 'Mayıs': "05", 'Haziran': "06", 'Temmuz': "07", 'Ağustos': "08", 'Eylül': "09", 'Ekim': "10", 'Kasım': "11", 'Aralık': "12"}
+
+# hepsiBuradaURL1 = "https://blackgate.hepsiburada.com/moriaapi/api/product-and-facet?pageType=Search&q=ar%C3%A7elik&size=24&sortType=yorumsayisi"
+hepsiBuradaURL1 = "https://blackgate.hepsiburada.com/moriaapi/api/product-and-facet?filter=categoryId:2147483637%3Bmarkalar:Ar%E2%82%ACC3%E2%82%ACA7elik&pageType=Category&size=24&sortType=yorumsayisi"
+# hepsiBuradaURL1 = "https://blackgate.hepsiburada.com/moriaapi/api/product-and-facet?filter=categoryId:2147483637&pageType=Category&size=24&sortType=yorumsayisi"
+# hepsiBuradaURL1 = "https://blackgate.hepsiburada.com/moriaapi/api/product-and-facet?filter=categoryId:2147483637%3Bmarkalar:miele&pageType=Category&size=24&sortType=yorumsayisi"
+hepsiBuradaURL2 = "https://user-content-gw-hermes.hepsiburada.com/queryapi/v2/ApprovedUserContents?skuList="
+
+# amazonURL1 = 'https://www.amazon.com.tr/s?k=ar%C3%A7elik&i=kitchen&bbn=12466781031&rh=n%3A12466781031%2Cp_89%3AAr%C3%A7elik&s=review-rank&dc&__mk_tr_TR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=1XO95E0RDIRCK&qid=1658674485&rnid=13493765031&sprefix=ar%C3%A7elik%2Caps%2C391&ref=sr_nr_p_89_2&ds=v1%3AowcA2EawmgCqf9%2FT3aVBxWC5NxolUxZfpL43gY7Duh8'
+# amazonURL1 = 'https://www.amazon.com.tr/s?k=beko&i=kitchen&page=3&crid=2U99HYJ3R1YKA&qid=1670183257&sprefix=beko%2Ckitchen%2C153&ref=sr_pg_3'
+amazonURL1 = 'https://www.amazon.com.tr/s?k=ar%C3%A7elik&s=review-rank&crid=S6OYEYZJGD4S&qid=1670264949&sprefix=%2Caps%2C176&ref=sr_st_review-rank&ds=v1%3AbB7k6BOQHM8YhMVXIN%2FQP2aDTUz6HNodKQfjOzcyg4Y'
